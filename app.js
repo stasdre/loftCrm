e6442b0414f117349dda659ccbaa68b2a5d27bbc
Stan <stasdre@gmail.com>
2019-10-12 14:02:15 +0300

    init

---
+++ app.js
@@ -0,0 +1,40 @@
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const httpErrors = require("http-errors");
+const logger = require("morgan");
+const path = require("path");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  // Handle React routing, return all requests to React app
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(httpErrors(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json(err);
+});
+
+module.exports = app;
---
+++ client/build/precache-manifest.1b201d2831143bad1b431fb5e61e35d2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8396c10e68570b59945a16c75558c1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "759941ebb6655e20cbe9",
+    "url": "/static/js/2.1db43b0f.chunk.js"
+  },
+  {
+    "revision": "550c0c1882add812d333",
+    "url": "/static/js/main.8f8d2a06.chunk.js"
+  },
+  {
+    "revision": "32996fb276b3e890a01d",
+    "url": "/static/js/runtime~main.01574ed8.js"
+  }
+]);
