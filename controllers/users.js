86f4f68ff1e90c3b7b7ad7fac9f3dc0337896f13
Stan <stasdre@gmail.com>
2019-10-13 17:53:17 +0300

    remove user

--- controllers/users.js
+++ controllers/users.js
@@ -41,7 +41,21 @@ exports.create = (req, res, next) => {
   });
 };
 
-exports.remove = (req, res, next) => {};
+exports.remove = (req, res, next) => {
+  User.deleteOne({ _id: req.params.id })
+    .then(results => {
+      if (results) {
+        return res
+          .status(200)
+          .json({ error: false, message: "User was deleted" });
+      }
+
+      return res.status(404).json({ error: true, message: "User not found" });
+    })
+    .catch(e => {
+      return res.status(400).json({ error: true, message: e.message });
+    });
+};
 
 exports.getAll = (req, res, next) => {
   User.find({}, { __v: 0 }).then(u => {
