f2f3ec35450c3846a7e03947f2e8137da37c4dde
Stan <stasdre@gmail.com>
2019-10-15 13:38:42 +0300

    news CRUD

--- api/v1.0/news/index.js
+++ api/v1.0/news/index.js
@@ -5,5 +5,7 @@ const router = Router();
 
 router.get("/", controller.getAll);
 router.post("/", controller.create);
+router.delete("/:id", controller.remove);
+router.patch("/:id", controller.update);
 
 module.exports = router;
--- controllers/news.js
+++ controllers/news.js
@@ -36,3 +36,43 @@ exports.create = (req, res, next) => {
       return res.status(400).json({ error: true, message: e.message });
     });
 };
+
+exports.remove = (req, res, next) => {
+  News.deleteOne({ _id: req.params.id })
+    .then(results => {
+      if (results) {
+        News.find()
+          .populate("user", "firstName image middleName surName username")
+          .then(n => {
+            const news = n.map(item => item.transform(true));
+            return res.status(200).json(news);
+          });
+      } else {
+        return res.status(404).json({ error: true, message: "News not found" });
+      }
+    })
+    .catch(e => {
+      return res.status(400).json({ error: true, message: e.message });
+    });
+};
+
+exports.update = (req, res, next) => {
+  const { text, title } = req.body;
+
+  News.updateOne({ _id: req.params.id }, { text, title })
+    .then(results => {
+      if (results) {
+        News.find()
+          .populate("user", "firstName image middleName surName username")
+          .then(n => {
+            const news = n.map(item => item.transform(true));
+            return res.status(200).json(news);
+          });
+      } else {
+        return res.status(404).json({ error: true, message: "News not found" });
+      }
+    })
+    .catch(e => {
+      return res.status(400).json({ error: true, message: e.message });
+    });
+};
