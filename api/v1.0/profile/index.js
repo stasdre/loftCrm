34ce85762fd40a61c50c870b19eb18f9b84c850d
Stan <stasdre@gmail.com>
2019-10-15 18:06:38 +0300

    patch user profile action

--- api/v1.0/profile/index.js
+++ api/v1.0/profile/index.js
@@ -1,8 +1,34 @@
 const { Router } = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 const controller = require("../../../controllers/profile");
 
 const router = Router();
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    if (
+      !fs.existsSync(path.join(__dirname, "../../../client/public/uploads"))
+    ) {
+      fs.mkdirSync(path.join(__dirname, "../../../client/public/uploads"), {
+        recursive: true
+      });
+    }
+
+    cb(null, path.join(__dirname, "../../../client/public/uploads"));
+  },
+  filename: (req, file, cb) => {
+    const fileName = `${Date.now()}-${file.originalname}`;
+    cb(null, fileName);
+  }
+});
+const upload = multer({
+  storage,
+  limits: { fileSize: 2000000 }
+});
+
 router.get("/", controller.check);
+router.patch("/", upload.single("avatar"), controller.update);
 
 module.exports = router;
--- controllers/profile.js
+++ controllers/profile.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const Jimp = require("jimp");
+const path = require("path");
+
 const User = mongoose.model("User");
 
 exports.check = (req, res, next) => {
@@ -19,4 +22,43 @@ exports.check = (req, res, next) => {
       message: "No token provided."
     });
   }
+};
+
+exports.update = (req, res, next) => {
+  const { firstName, middleName, surName, oldPassword, newPassword } = req.body;
+  const file = req.file;
+
+  User.findById(req.token.id)
+    .then(async u => {
+      if (newPassword) {
+        if (u.validPassword(oldPassword)) {
+          u.setPassword(newPassword);
+        } else {
+          return res.status(400).send({
+            error: true,
+            message: "Incorrect password"
+          });
+        }
+      }
+      u.firstName = firstName;
+      u.middleName = middleName;
+      u.surName = surName;
+      if (file) {
+        await Jimp.read(path.join(file.path))
+          .then(image => {
+            image
+              .resize(400, 400)
+              .quality(90)
+              .write(file.path);
+            u.image = `/uploads/${file.filename}`;
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      }
+      u.save()
+        .then(data => res.status(200).json({ ...data.transform() }))
+        .catch(e => res.status(400).json({ error: true, message: e.message }));
+    })
+    .catch(e => res.status(400).json({ error: true, message: e.message }));
 };
