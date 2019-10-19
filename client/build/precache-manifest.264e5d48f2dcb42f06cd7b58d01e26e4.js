566b6fa6199dca06d7bfb638b29ef146e030d372
Stan <stasdre@gmail.com>
2019-10-19 17:46:07 +0300

    add build to repo

---
+++ client/build/precache-manifest.264e5d48f2dcb42f06cd7b58d01e26e4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f088a90ac1296d649e3d011ca8842803",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdaf2215d695a8dbf112",
+    "url": "/static/js/2.3825e85b.chunk.js"
+  },
+  {
+    "revision": "b3169bbd7e5bc3a438b3",
+    "url": "/static/js/main.f9db47f1.chunk.js"
+  },
+  {
+    "revision": "b87d381de152b7d79852",
+    "url": "/static/js/runtime~main.d9ba5c58.js"
+  }
+]);
