7f5f884dcf6a1f539ac65549f3f09b3ce117cf28
Stan <stasdre@gmail.com>
2019-10-19 11:09:27 +0300

    remove checkToken for refresh

--- api/v1.0/auth.js
+++ api/v1.0/auth.js
@@ -1,12 +1,11 @@
 const { Router } = require("express");
 const controller = require("../../controllers/auth");
-const checkToken = require("../../middleware/checkToken");
 const schema = require("../../schemas/reg");
 const validation = require("../../middleware/validation");
 
 const router = Router();
 
-router.post("/refresh-token", checkToken, controller.refresh);
+router.post("/refresh-token", controller.refresh);
 router.post("/login", controller.login);
 router.post("/registration", validation(schema), controller.reg);
 
